983d2f7ef8618953e91db2c3ec97c5034b0b7514
Juan Pablo <juanpabloqb1990@gmail.com>
2025-01-15 16:04:29 -0500

    logo fixed 2.0

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,24 +1,21 @@
-import Logo from "@/components/Logo"
-import { Outlet } from "react-router-dom"
-import { ToastContainer } from "react-toastify"
+import Logo from "@/components/Logo";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const AuthLayout = () => {
   return (
     <>
-        <div className="bg-gray-800 min-h-screen">
-            <div className="py-10 lg:py-20 mx-auto w-[450px]">
-                <Logo />
-                <div className="mt-10">
-                    <Outlet />
-                </div>
-            </div>
+      <div className="bg-gray-800 min-h-screen">
+        <div className="py-10 lg:py-20 mx-auto w-[450px]">
+          <Logo />
+          <div className="mt-10">
+            <Outlet />
+          </div>
         </div>
-        <ToastContainer
-            pauseOnFocusLoss={false}
-            pauseOnHover={false}
-        />
+      </div>
+      <ToastContainer pauseOnFocusLoss={false} pauseOnHover={false} />
     </>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
